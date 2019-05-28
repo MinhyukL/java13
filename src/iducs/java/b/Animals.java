7c3cd11bea91ec53cfa54cf1b8ecaeeb53645eda
MinhyukL <mzerin@naver.com>
2019-05-28 17:20:56 +0900

    new file and revise code

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -1,8 +1,10 @@
 package iducs.java.b;
 
 public interface Animals {
-	void move();
-	void eat();
+	public final String species = "µ¿¹°"; // public final
+	
+	void move(); // public abstract
+	void eat(); 
 	void sound();
 	void breathe();
 }
